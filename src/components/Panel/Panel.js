28bf84daf3d51cdaf2e1fe54d8ef2ef1e834ddea
Manuel Entrena <manuel.entrena@gmail.com>
2021-05-02 12:51:32 +0200

    Añadimos estado de colores al restante

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -3,10 +3,24 @@ import "./panel.css";
 
 const Panel = ({ credito, restante }) => {
   const [oculto, setOculto] = useState("oculto");
-
+  const [color, setColor] = useState("verde");
   useEffect(() => {
+    /* debugger; */
+    /* Calculamos si mostrar el panel o no */
     credito !== 0 ? setOculto("") : setOculto("oculto");
-  }, [credito]);
+    /* calculamos el color del restante */
+
+    const porcentaje = (restante * 100) / credito;
+    if (porcentaje <= 100) {
+      setColor("verde");
+    }
+    if (porcentaje < 50) {
+      setColor("naranja");
+    }
+    if (porcentaje < 25) {
+      setColor("rojo");
+    }
+  }, [credito, restante]);
 
   return (
     <div className={"panel " + oculto}>
@@ -16,7 +30,7 @@ const Panel = ({ credito, restante }) => {
       </div>
       <div className="panel__bloque">
         <p className="panel__title">Restante: </p>
-        <p className="panel__result naranja">{restante}€</p>
+        <p className={"panel__result " + color}>{restante}€</p>
       </div>
     </div>
   );
